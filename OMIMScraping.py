149bb58082762f4570c60fc1bb18465677d96c28
AvinWangZH <avin.wangzihan@gmail.com>
2016-07-13 16:18:11 -0400

    Add more functions

--- OMIMScraping.py
+++ OMIMScraping.py
@@ -1,6 +1,12 @@
 from urllib.request import urlopen
 import json
 import logging
+import re
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
 
 def omim_page_scraping(start_num, limit):
     '''
@@ -24,6 +30,7 @@ def get_total_results():
     total_results = json_data['omim']['searchResponse']['totalResults']    
     return total_results
 
+
 def omim_reference_scrape_full():
     
     #The code below is used to scrape data from the OMIM website by using its API
@@ -45,10 +52,28 @@ def omim_reference_scrape_full():
     
     with open('new_entry_list.json', 'w') as file:
         json.dump(new_entry_list, file)
+        
+        
+def rearrange_omim_info():
     
-   
-
-if __name__ == '__main__':
+    '''
+    This function is used to rearrange the data in the omim dictionary.
+    The data in the new dictionary ('omim_full_dict.json') has a format of
+    
+    OMIMid/
+         - title   (the name of the disease)
+         - pubList/
+           - pubmedID/
+             - journal
+             - title
+             - author
+    
+    This dictionary is stored in the computer directly.
+    
+    input: None
+    output: None
+                
+    '''
     
     omim_dict = {}
     count = 0   #count number of articles not in Pubmed
@@ -61,21 +86,61 @@ if __name__ == '__main__':
     
     for entry in entry_list:
         omim_dict[entry['entry']['mimNumber']] = {}
+        omim_dict[entry['entry']['mimNumber']]['title'] = entry['entry']['titles']['preferredTitle']
         print('count entry: ' + str(count1))
-        pub_list = []
+        pub_dict = {}
         count1 += 1
         for pub in entry['entry']['referenceList']:
             if 'pubmedID' in pub['reference'].keys():
-                count2 += 1
-                pub_list.append(pub['reference']['pubmedID'])
-                omim_dict[entry['entry']['mimNumber']]['Publications'] = pub_list
-            else:
-                count += 1
+                #count2 += 1
+                pub_dict[pub['reference']['pubmedID']] = {'journal': pub['reference']['source'], 'title': pub['reference']['title'], 'authors': pub['reference']['authors']}
+            #else:
+                #count += 1
+        omim_dict[entry['entry']['mimNumber']]['pubList'] = pub_dict
+    
+    with open('omim_full_dict.json', 'w') as new_file:
+        json.dump(omim_dict, new_file)
+    
+    return
+
+def journal_date_parse(string):
+    
+    source_re = re.compile(r'((?:[a-zA-Z]+\s+)+).+(\d{4}).*')
+    match = source_re.match(string)
+    journal_list = []
+    if match:
+        journal = match.group(1).strip()
+        date = int(match.group(2))
+        journal_list.append(journal)
+        journal_list.append(date)
+    return journal_list
+
+if __name__ == '__main__':
+    
+    with open('omim_full_dict.json', 'r') as file:
+        omim_dict = json.load(file)
+        
+    journal_list = []
+    date_list = []
+    
+    for omim in omim_dict:
+        for pub in omim_dict[omim]['pubList']:
+            journal = []
+            journal = journal_date_parse(omim_dict[omim]['pubList'][pub]['journal'])
+            if journal:
+                journal_list.append(journal[0])
+                date_list.append(journal[1])
+                
+    journal_distribution = Counter(journal_list)
+    date_distribution = Counter(date_list)
+                
+    
+                
+
         
     
     
-      
-    
+
 
         
     
